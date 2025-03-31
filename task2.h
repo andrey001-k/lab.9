f4107490130f22c612733c71e33bd75e26b416f7
ShanhinAndrey <a.a.shanhin@student.khai.edu>
2025-03-31 10:15:53 +0300

    task2.h

---
+++ task2.h
@@ -0,0 +1,9 @@
+#ifndef TASK2_H
+#define TASK2_H
+
+#include <string>
+
+void append_chars(std::string& str, const std::string& add, size_t n);
+bool is_valid_string(const std::string& str);
+
+#endif // TASK2_H
